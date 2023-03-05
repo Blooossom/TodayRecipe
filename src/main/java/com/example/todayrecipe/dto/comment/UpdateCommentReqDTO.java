fa177641c09e4d43cfd001e40e8f6f231a1066b4
Blooossom <taegyeong19@gmail.com>
2023-03-05 18:48:47 +0900

    #4 댓글 수정 DTO 추가

---
+++ src/main/java/com/example/todayrecipe/dto/comment/UpdateCommentReqDTO.java
@@ -0,0 +1,14 @@
+package com.example.todayrecipe.dto.comment;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UpdateCommentReqDTO {
+
+    private String content;
+
+}
