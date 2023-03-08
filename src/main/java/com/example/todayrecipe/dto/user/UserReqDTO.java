62e91dd52cfba5c244be54bb8a60f4bffbd304c3
Blooossom <taegyeong19@gmail.com>
2023-03-08 09:55:19 +0900

    #3 Entity 필드 변경에 따른 DTO 수정

--- src/main/java/com/example/todayrecipe/dto/user/UserReqDTO.java
+++ src/main/java/com/example/todayrecipe/dto/user/UserReqDTO.java
@@ -20,7 +20,9 @@ import java.util.Collections;
 @Builder
 public class UserReqDTO {
 
-
+    @NotBlank(message = "이메일은 필수 입력 사항입니다.")
+    //@Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
+    private String email;
     @NotBlank(message = "비밀번호는 필수 입력 사항입니다.")
     //@Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8 ~ 16글자의 영문 대소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
@@ -29,20 +31,24 @@ public class UserReqDTO {
     //@Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$}", message = "닉네임은 특수문자를 제외한 2 ~ 10자리로 이루어져야 합니다.")
     private String nickname;
 
-    @NotBlank(message = "이메일은 필수 입력 사항입니다.")
-    //@Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
-    private String email;
+    private String name;
+
+    private String phone;
 
     @NotBlank(message = "주소는 필수 입력 사항입니다.")
     private String address;
+
     private Post post;
+
     private Comment comment;
 
     public User toEntity() {
         return User.builder()
-                .password(password)
-                .nickname(nickname)
                 .email(email)
+                .password(password)
+                .name(name)
+                .nickname(nickname)
+                .phone(phone)
                 .address(address)
                 .build();
     }
--- src/main/java/com/example/todayrecipe/dto/user/UserResDTO.java
+++ src/main/java/com/example/todayrecipe/dto/user/UserResDTO.java
@@ -13,15 +13,26 @@ import lombok.*;
 public class UserResDTO {
 
     private Long id;
-    private String nickname;
+
     private String email;
+
+    private String nickname;
+
+    private String name;
+
+    private String phone;
     private String indate;
+
     private Post post;
+
     private Comment comment;
 
     public UserResDTO(User user){
         this.id = user.getId();
         this.email = user.getEmail();
+        this.name = user.getName();
+        this.nickname = user.getNickname();
+        this.phone = user.getPhone();
         this.indate = user.getIndate();
     }
 }
