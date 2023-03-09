3adef3eaeb4984ba0bd8394c484afe3da83def07
Blooossom <taegyeong19@gmail.com>
2023-03-09 21:16:48 +0900

    #22 Controller 내부 메소드 추가

--- src/main/java/com/example/todayrecipe/controller/MyPageController.java
+++ src/main/java/com/example/todayrecipe/controller/MyPageController.java
@@ -2,12 +2,14 @@ package com.example.todayrecipe.controller;
 
 import com.example.todayrecipe.dto.user.LoginReqDTO;
 import com.example.todayrecipe.dto.user.UpdateUserReqDTO;
+import com.example.todayrecipe.dto.user.UserResDTO;
 import com.example.todayrecipe.service.AuthService;
 import com.example.todayrecipe.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +29,11 @@ public class MyPageController {
     public ResponseEntity<String> updateUserInfo(@AuthenticationPrincipal LoginReqDTO user, @RequestBody UpdateUserReqDTO updateUserReqDTO) {
         return authService.updateUserInfo(updateUserReqDTO, user);
     }
+    @GetMapping("/mypage/myinfo")
+    public ResponseEntity<UserResDTO> viewMyInfo(@AuthenticationPrincipal LoginReqDTO reqDTO) {
+        return authService.viewMyInfo(reqDTO);
+    }
+
 
 
 
