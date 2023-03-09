90d1f320229f175558c8b1c571ec851955a51be0
Blooossom <taegyeong19@gmail.com>
2023-03-09 18:41:16 +0900

    #13 UpdatePost 파라미터 수정
    PostReqDTO -> UpdatePostReqDTO 수정

--- src/main/java/com/example/todayrecipe/controller/PostController.java
+++ src/main/java/com/example/todayrecipe/controller/PostController.java
@@ -1,13 +1,13 @@
 package com.example.todayrecipe.controller;
 
 
-import com.example.todayrecipe.dto.post.PostRequest;
-import com.example.todayrecipe.dto.post.PostResponse;
+import com.example.todayrecipe.dto.post.PostReqDTO;
+import com.example.todayrecipe.dto.post.PostResDTO;
+import com.example.todayrecipe.dto.post.UpdatePostReqDTO;
 import com.example.todayrecipe.dto.user.LoginReqDTO;
 import com.example.todayrecipe.service.PostService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,7 +27,7 @@ public class PostController {
      * 추천 게시글 조회
      */
     @GetMapping("/recommendRecipeList")
-    public List<PostResponse> viewRecommendRecipe() {
+    public List<PostResDTO> viewRecommendRecipe() {
         return service.selectRecommendList();
     }
 
@@ -35,7 +35,7 @@ public class PostController {
      * 내가 작성한 게시글 목록 출력
      */
     @GetMapping("/goMyPostList")
-    public List<PostResponse> viewPostListByUser(@AuthenticationPrincipal LoginReqDTO user){
+    public List<PostResDTO> viewPostListByUser(@AuthenticationPrincipal LoginReqDTO user){
         return service.selectPostListByEmail(user);
     }
 
@@ -44,7 +44,7 @@ public class PostController {
      */
     @ApiOperation(value = "게시글 전체 조회", notes = "게시글 전부 가져오는 API")
     @GetMapping("/viewPostList")
-    public List<PostResponse> viewPostList(){
+    public List<PostResDTO> viewPostList(){
         return service.selectPostList();
     }
 
@@ -53,7 +53,7 @@ public class PostController {
      */
     @ApiOperation(value = "게시글 검색", notes = "키워드에 따라 게시글을 검색하는 API")
     @GetMapping("/post/search?keyword={keyword}")
-    public List<PostResponse> viewPostListByNickName(HttpSession session) {
+    public List<PostResDTO> viewPostListByNickName(HttpSession session) {
         session.setAttribute("post_id", null);
         return null;
     }
@@ -62,17 +62,16 @@ public class PostController {
      * 게시글 작성
      */
     @ApiOperation(value = "게시글 작성", notes = "게시글을 작성하는 API")
-    @PostMapping("/addRecipe")
-    public ResponseEntity<String> writePost(@AuthenticationPrincipal LoginReqDTO user, @RequestBody PostRequest request) {
+    @PostMapping("/post")
+    public ResponseEntity<String> writePost(@AuthenticationPrincipal LoginReqDTO user, @RequestBody PostReqDTO request) {
         return service.addPost(request, user);
     }
 
     /**
      * 게시글 삭제
      */
-    @Transactional
     @ApiOperation(value = "게시글 삭제", notes = "게시글을 삭제하는 API")
-    @DeleteMapping("/deletePost")
+    @DeleteMapping("/post")
     public ResponseEntity<String> deletePost(@RequestBody HashMap<String, Object> map, @AuthenticationPrincipal LoginReqDTO user){
         return service.deletePost(map, user);
     }
@@ -81,7 +80,7 @@ public class PostController {
      * 게시글 이동
      */
     @GetMapping("/selectPost")
-    public ResponseEntity<PostResponse> selectPost(@RequestBody HashMap<String, Object> map){
+    public ResponseEntity<PostResDTO> selectPost(@RequestBody HashMap<String, Object> map){
         return service.viewPost(map);
     }
 
@@ -90,9 +89,9 @@ public class PostController {
      */
     @Transactional
     @ApiOperation(value = "게시글 수정", notes = "게시글을 수정하는 API")
-    @PutMapping("/updatePost")
-    public ResponseEntity<String> updatePost(@AuthenticationPrincipal LoginReqDTO user, @RequestBody PostRequest request) {
-        return service.updatePost(request, user);
+    @PutMapping("/post")
+    public ResponseEntity<String> updatePost(@AuthenticationPrincipal LoginReqDTO user, @RequestBody UpdatePostReqDTO reqDTO) {
+        return service.updatePost(reqDTO, user);
     }
 
     /**
