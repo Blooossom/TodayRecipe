908525595008d628d09844808ef7c2f237c2c86c
Blooossom <taegyeong19@gmail.com>
2023-03-09 21:46:39 +0900

    #16 컨트롤러 파라미터 수정

--- src/main/java/com/example/todayrecipe/controller/CommentController.java
+++ src/main/java/com/example/todayrecipe/controller/CommentController.java
@@ -1,23 +1,20 @@
 package com.example.todayrecipe.controller;
 
 import com.example.todayrecipe.dto.comment.CommentReqDTO;
-import com.example.todayrecipe.dto.comment.CommentResponse;
+import com.example.todayrecipe.dto.comment.CommentResDTO;
 import com.example.todayrecipe.dto.comment.UpdateCommentReqDTO;
 import com.example.todayrecipe.dto.user.LoginReqDTO;
-import com.example.todayrecipe.entity.Comment;
 import com.example.todayrecipe.service.CommentService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.List;
 
@@ -34,15 +31,15 @@ public class CommentController {
     @ApiOperation(value = "댓글 출력", notes = "게시글과 함께 댓글을 가져오는 API")
     @ApiImplicitParam(name = "post_id", value = "게시글 식별 ID", required = true)
     @GetMapping("/viewComment")
-    public List<CommentResponse> selectComment(@RequestBody HashMap<String, Object> map){
+    public List<CommentResDTO> selectComment(@RequestBody HashMap<String, Object> map){
         return service.viewCommentList(map);
     }
 
     /**
      * 내가 작성한 댓글 조회
      */
-    @GetMapping("/goMyCommentList")
-    public List<CommentResponse> selectMyCommentList(@AuthenticationPrincipal LoginReqDTO user) {
+    @GetMapping("/mypage/commentlist")
+    public List<CommentResDTO> selectMyCommentList(@AuthenticationPrincipal LoginReqDTO user) {
         return service.viewMyComment(user);
     }
 
@@ -54,19 +51,19 @@ public class CommentController {
             @ApiImplicitParam(name = "user_id", value = "유저 식별 ID", required = true),
             @ApiImplicitParam(name = "post_id", value = "게시글 식별 ID", required = true)
     })
-    @PostMapping("/addComment")
-    public ResponseEntity<String> addComment(@AuthenticationPrincipal LoginReqDTO user, @RequestBody CommentReqDTO request, @RequestBody HashMap<String, Object> map) {
-        return service.addComment(request, user, map);
+    @PostMapping("/comment")
+    public ResponseEntity<String> addComment(@AuthenticationPrincipal LoginReqDTO user, @RequestBody CommentReqDTO request) {
+        return service.addComment(request, user);
     }
 
     @ApiOperation(value = "댓글 수정", notes = "댓글 수정하는 API")
-    @PutMapping("/updateComment")
+    @PutMapping("/comment")
     public ResponseEntity<String> modifyComment(@RequestBody UpdateCommentReqDTO reqDTO, @RequestBody HashMap<String, Object> map){
         return service.updateComment(reqDTO, map);
     }
     @Transactional
     @ApiOperation(value = "댓글 삭제", notes = "댓글을 삭제하는 API")
-    @DeleteMapping("/deleteComment")
+    @DeleteMapping("/comment")
     public ResponseEntity<String> deleteComment(@RequestBody HashMap<String, Object> map, @AuthenticationPrincipal LoginReqDTO user){
         return service.deleteComment(map, user);
     }
