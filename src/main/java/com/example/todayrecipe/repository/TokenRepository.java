c50d9adf8cf4940bd286d6358d13cd4f7ac36c6c
Blooossom <taegyeong19@gmail.com>
2023-03-15 21:11:58 +0900

    Refactor :  불필요한 Repository 삭제 - #10

--- src/main/java/com/example/todayrecipe/repository/TokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.todayrecipe.repository;
-
-import com.example.todayrecipe.entity.Blacklist;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TokenRepository extends JpaRepository<Blacklist, Long> {
-
-    boolean existsByToken(String token);
-}
